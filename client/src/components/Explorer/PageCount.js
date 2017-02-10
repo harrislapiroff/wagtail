30a035fe59447b9ce0f69e0cf8e8189bf3cc76e3
Harris Lapiroff <harris@chromamine.com>
2017-02-10 13:50:48 +0000

    Add "Explore all in Page" button to explorer
    This makes the UX usable in cases where there are more child pages
    than the API delivers in one request

---
+++ client/src/components/Explorer/PageCount.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { ADMIN_URLS, STRINGS } from 'config/wagtail';
+
+const PageCount = ({ id, count, title }) => (
+  <div
+    onClick={() => {
+      window.location.href = `${ADMIN_URLS.PAGES}${id}/`;
+    }}
+    className="c-explorer__see-more"
+  >
+    {STRINGS.EXPLORE_ALL_IN}{' '}
+    <span className="c-explorer__see-more__title">{title}</span>{' '}
+    ({count} {count !== 1 ? STRINGS.PAGES : STRINGS.PAGE})
+  </div>
+);
+
+PageCount.propTypes = {
+  id: React.PropTypes.number.isRequired,
+  count: React.PropTypes.number.isRequired,
+  title: React.PropTypes.string.isRequired,
+};
+
+export default PageCount;
